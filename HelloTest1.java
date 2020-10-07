87c25503c2483714805d94213c5c478a2f3f6b6a
Alexander Crabtree <70244225+crabta4490@users.noreply.github.com>
2020-10-07 00:13:37 -0400

    Update HelloTest1.java
    I added an update on line 3 and modified the comments on line 8

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
+ * Updated on 10/7/20 by AC
  * Created on 8/3/18 by AJT
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull Request
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
